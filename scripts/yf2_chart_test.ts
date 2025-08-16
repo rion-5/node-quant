d29641f994805d36cdb147303c4b03a8009eeb95
rion-5 <sklee@hanyang.ac.kr>
2025-08-16 11:54:44 +0900

    edit yf2_test

--- scripts/yf2_chart_test.ts
+++ scripts/yf2_chart_test.ts
@@ -13,5 +13,19 @@ interface StockData {
 async function getStockQuote(symbol: string){
   try{
     const quote = await yahooFinance.quote(symbol);
+    console.log(`${symbol} 현재 정보:`);
+    console.log(`현재가: $${quote.regularMarketPrice}`);
+    console.log(`전일 종가: $${quote.regularMarketPreviousClose}`);
+
+    return quote;
+  }catch (error) {
+    console.error('Quote 가져오기 실패:', error);
+    throw error;
   }
 }
+
+async function main(){
+  getStockQuote('AAPL');
+}
+
+main();
