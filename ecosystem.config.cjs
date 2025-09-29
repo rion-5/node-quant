b768307ebf9558774693b11c0b764ac4386d2b04
rion-5 <sklee@hanyang.ac.kr>
2025-09-29 15:50:51 +0900

    edit ecosystem.config.cjs args: 'run start'

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
   apps: [
     {
       name: 'node-quant',
-      // script: 'build/index.js', // 또는 'build', 아래 참고
-      // cwd: './', // 프로젝트 루트 기준
-      // interpreter: 'node',
-      script: 'start',
-      interpreter: 'npm',
+      // PM2가 실행할 명령어를 직접 지정합니다.
+      script: 'npm', 
+      args: 'run start', // 'npm' 뒤에 오는 인수로 'run start'를 전달합니다.
+      // interpreter는 필요 없거나 'node'로 설정해도 됩니다.
+      
       env: {
         NODE_ENV: 'development',
         PORT: 5173
